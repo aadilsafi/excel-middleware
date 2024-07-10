364bad84ba5d659a21ebf90b6b426d27b2d99e14
Aadil Safi <aadilxafi@gmail.com>
2024-07-10 23:52:20 +0500

    added log files for try catch and other file path added to log

--- app/Console/Commands/ProcessOrderFiles.php
+++ app/Console/Commands/ProcessOrderFiles.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Storage;
 use App\Mail\FilesReport;
 use App\Models\Order;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Support\Facades\Log;
 
 class ProcessOrderFiles extends Command
@@ -22,78 +23,84 @@ class ProcessOrderFiles extends Command
 
     public function handle()
     {
-        Log::info('processing ftp files for orders command');
-        $sellerCloudService = new \App\Services\SellerCloudService();
+        try {
 
-        // Define the FTP directory
-        $directory = 'eo/outgoing';
-        $files = Storage::disk('rsr')->files($directory);
+            Log::info('processing ftp files for orders command');
+            $sellerCloudService = new \App\Services\SellerCloudService();
 
-        if (empty($files)) {
-            $this->info('No files found.');
-            return;
-        }
+            // Define the FTP directory
+            $directory = 'eo/outgoing';
+            $files = Storage::disk('rsr')->files($directory);
 
-        foreach ($files as $file) {
-            $filePath = storage_path('app/' . $file);
-            $ftp_file = Storage::disk('rsr')->get($file);
-
-            $local_file = Storage::disk('local')->put($file, $ftp_file);
-
-            $file_content = Storage::disk('local')->get($file);
-            $attachment = [
-                'path' => $filePath,
-                'content' => $file_content,
-                'name' => basename($file),
-                'mime' => Storage::disk('local')->mimeType($file) ?? 'text/plain',
-            ];
-            // Send email
-            if (strpos($filePath, 'ECONF') !== false) {
-                // Just delete
-            } else if (\strpos($filePath, 'ESHIP') !== false) {
-                $tracker = $this->getTracking($file_content);
-                $tracking_number = $tracker[0];
-                $invoice_number  = $tracker[1];
-                $order_id = $tracker[2];
-                // $order_id = Order::where('order_source_id', $source_id)->first()->order_id;
-                // Output the extracted data
-                if ($tracking_number && $invoice_number) {
-                    $this->info("Tracking Number: $tracking_number");
-                    $this->info("Invoice Number: $invoice_number");
-                    Log::info('Tracking : ' . $tracking_number);
-                    Log::info('Tracking : ' . $invoice_number);
-
-                    // Get date from file name
-                    preg_match('/(\d{8})/', basename($file), $matches);
-                    if (isset($matches[1])) {
-                        $file_date = Carbon::createFromFormat('Ymd', $matches[1]);
-                    } else {
-                        $file_date = Carbon::now();
-                    }
-                    $ship_date = Carbon::parse($file_date)->format('Y-m-d\TH:i:s.v\Z');
-                    // last column is warehouse id default is 255 for RSR Dropship
-                    $sellerCloudService->updateShipping($order_id, $ship_date, $tracking_number, 'FedEx', 'FedEx 2Day');
-                } else {
-                    $this->error('Tracking number or invoice number not found.');
-                }
-            } else {
-                $tracker = $this->getTracking($file_content);
-                $order_id = $tracker[2];
-                $sellerCloudService->sendEmail($attachment, ['heading' => 'Order File Error on OrderId => '.$order_id, 'body' => 'Order file attached on OrderId => '.$order_id, 'title' => 'Order File on OrderId => '.$order_id]);
-                // Mail::to('test@test.com')->send(new FilesReport($attachment));
+            if (empty($files)) {
+                $this->info('No files found.');
+                return;
             }
-            Storage::disk('local')->delete($file);
-            Storage::disk('rsr')->delete($file);
+
+            foreach ($files as $file) {
+                $filePath = storage_path('app/' . $file);
+                $ftp_file = Storage::disk('rsr')->get($file);
+                Log::info('processing order file :'.$filePath);
+                $local_file = Storage::disk('local')->put($file, $ftp_file);
+
+                $file_content = Storage::disk('local')->get($file);
+                $attachment = [
+                    'path' => $filePath,
+                    'content' => $file_content,
+                    'name' => basename($file),
+                    'mime' => Storage::disk('local')->mimeType($file) ?? 'text/plain',
+                ];
+                // Send email
+                if (strpos($filePath, 'ECONF') !== false) {
+                    // Just delete
+                } else if (\strpos($filePath, 'ESHIP') !== false) {
+                    $tracker = $this->getTracking($file_content);
+                    $tracking_number = $tracker[0];
+                    $invoice_number  = $tracker[1];
+                    $order_id = $tracker[2];
+                    // $order_id = Order::where('order_source_id', $source_id)->first()->order_id;
+                    // Output the extracted data
+                    if ($tracking_number && $invoice_number) {
+                        $this->info("Tracking Number: $tracking_number");
+                        $this->info("Invoice Number: $invoice_number");
+                        Log::info('Tracking : ' . $tracking_number);
+                        Log::info('Tracking : ' . $invoice_number);
+
+                        // Get date from file name
+                        preg_match('/(\d{8})/', basename($file), $matches);
+                        if (isset($matches[1])) {
+                            $file_date = Carbon::createFromFormat('Ymd', $matches[1]);
+                        } else {
+                            $file_date = Carbon::now();
+                        }
+                        $ship_date = Carbon::parse($file_date)->format('Y-m-d\TH:i:s.v\Z');
+                        // last column is warehouse id default is 255 for RSR Dropship
+                        $sellerCloudService->updateShipping($order_id, $ship_date, $tracking_number, 'FedEx', 'FedEx 2Day');
+                    } else {
+                        $this->error('Tracking number or invoice number not found.');
+                    }
+                } else {
+                    $tracker = $this->getTracking($file_content);
+                    $order_id = $tracker[2];
+                    $sellerCloudService->sendEmail($attachment, ['heading' => 'Order File Error on OrderId => ' . $order_id, 'body' => 'Order file attached on OrderId => ' . $order_id, 'title' => 'Order File on OrderId => ' . $order_id]);
+                    // Mail::to('test@test.com')->send(new FilesReport($attachment));
+                }
+                Storage::disk('local')->delete($file);
+                Storage::disk('rsr')->delete($file);
+            }
+
+
+
+            // Delete files
+            // foreach ($files as $file) {
+            //     // Storage::disk('rsr')->delete($file);
+            // }
+
+            $this->info('Files have been processed, emailed, and deleted successfully.');
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            $this->error('An error occurred while processing the files.');
         }
-
-
-
-        // Delete files
-        // foreach ($files as $file) {
-        //     // Storage::disk('rsr')->delete($file);
-        // }
-
-        $this->info('Files have been processed, emailed, and deleted successfully.');
     }
 
     public function getTracking($fileContent, $is_error = false)
