1f276ec31370f496b9f85116950b4bfe36b298b5
Aadil Safi <aadilxafi@gmail.com>
2024-10-14 22:27:00 +0500

    fixed the times

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,13 +13,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('order-files-process')->hourlyAt(01)->withoutOverlapping();
+        $schedule->command('order-files-process')->hourlyAt(10)->withoutOverlapping();
         $schedule->command('order-files-process-secondary')->hourlyAt(15)->withoutOverlapping();
         $schedule->command('get-vendor-products')->weeklyOn(1,'8:00')->withoutOverlapping();
         $schedule->command('get-vendor-products', [15080])->weeklyOn(1,'9:00')->withoutOverlapping();
         $schedule->command('get-vendor-products', [15391])->weeklyOn(1,'10:00')->withoutOverlapping();
-        $schedule->command('read-emails')->hourlyAt(30)->withoutOverlapping();
-        $schedule->command('process-seawide-orders')->everyTwoHours(50)->withoutOverlapping();
+        $schedule->command('read-emails')->hourlyAt(40)->withoutOverlapping();
+        $schedule->command('process-seawide-orders')->everyTwoHours()->withoutOverlapping();
         $schedule->command('app:new-orders-excel')->everyThirtyMinutes()->withoutOverlapping();
     }
 
